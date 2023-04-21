ebc3d59bd095e1e26b2ec048ee5482fd16ad40bd
louis cobigo <louis.cobigo@gmail.com>
2023-04-21 13:25:14 +0200

    add stock

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 // Initialize Express
 const app = express();
 
-let stock = []
+let stock = [];
 // Create GET request
 app.get("/api/ping", (req, res) => {
   res.send("PONG");
@@ -13,31 +13,33 @@ app.get("/api/ping", (req, res) => {
 // Endpoint pour accepter des marchandises dans le stock
 app.post("/api/stock/:productId/movement", async (req, res) => {
   const { productId } = req.params;
-  const { quantity } = req.body
+  const { quantity } = req.body;
 
   // Vérifier que le produit existe dans le catalogue
   try {
-    const response = await fetch(`http://microservices.tp.rjqu8633.odns.fr/api/products/${productId}`);
+    const response = await fetch(
+      `http://microservices.tp.rjqu8633.odns.fr/api/products/${productId}`
+    );
     const product = await response.json();
-    // const { quantity, status } = req.body as StockMovementDto;
-    console.log(product);
 
     // Si le produit existe, ajouter la quantité fournie au stock
     // Si le produit n'existe pas, renvoyer une erreur
     if (product) {
       // TODO : Ajouter la quantité fournie au stock
-      const index = stock.findIndex(item => item.productId === productId);
+      const index = stock.findIndex((item) => item.productId === productId);
       if (index !== -1) {
         // Le produit est déjà connu du stock, ajouter la quantité fournie à la quantité en stock
         stock[index].quantity += quantity;
       } else {
         // Le produit n'est pas connu du stock, ajouter une nouvelle entrée pour le produit avec la quantité fournie
+        //const newProductId = productId
+        //const newQuantity = quantity
         stock.push({ productId, quantity });
       }
-      res.status(200).send({"le produit est ajouté" : product, "stock" : stock});
+      res.status(200).send({ "le produit est ajouté": product, "stock": stock });
       //res.status(204).send()
     } else {
-      res.status(400).send("Le produit n\'existe pas dans le catalogue");
+      res.status(400).send("Le produit n'existe pas dans le catalogue");
     }
   } catch (error) {
     res.status(500).send("Erreur lors de la vérification du produit");
