7697ac783cc5e362dbae6b3193102e6e94a81d22
Manon Lavenier <manon.lavenier@gmail.com>
2023-04-21 11:26:19 +0200

    Comment

--- index.js
+++ index.js
@@ -11,27 +11,27 @@ app.get("/api/ping", (req, res) => {
 });
 
 // Endpoint pour accepter des marchandises dans le stock
-app.post('/stock/:productId/movement', async (req, res) => {
-  const { productId } = req.params;
-  const { quantity, status } = req.body // as StockMovementDto;
+// app.post('/stock/:productId/movement', async (req, res) => {
+//   const { productId } = req.params;
+//   const { quantity, status } = req.body // as StockMovementDto;
 
-  // Vérifier que le produit existe dans le catalogue
-  try {
-    const response = await fetch(`http://microservices.tp.rjqu8633.odns.fr/api/products/${productId}`);
-    const product = await response.json();
+//   // Vérifier que le produit existe dans le catalogue
+//   try {
+//     const response = await fetch(`http://microservices.tp.rjqu8633.odns.fr/api/products/${productId}`);
+//     const product = await response.json();
 
-    // Si le produit existe, ajouter la quantité fournie au stock
-    // Si le produit n'existe pas, renvoyer une erreur
-    if (product) {
-      // TODO : Ajouter la quantité fournie au stock
-      res.status(204).send();
-    } else {
-      res.status(400).send('Le produit n\'existe pas dans le catalogue');
-    }
-  } catch (error) {
-    res.status(500).send('Erreur lors de la vérification du produit');
-  }
-});
+//     // Si le produit existe, ajouter la quantité fournie au stock
+//     // Si le produit n'existe pas, renvoyer une erreur
+//     if (product) {
+//       // TODO : Ajouter la quantité fournie au stock
+//       res.status(204).send();
+//     } else {
+//       res.status(400).send('Le produit n\'existe pas dans le catalogue');
+//     }
+//   } catch (error) {
+//     res.status(500).send('Erreur lors de la vérification du produit');
+//   }
+// });
 
 // Initialize server
 app.listen(5000, () => {
