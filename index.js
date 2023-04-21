70d05ac30d49217447c42e7f8b5b424b9afdf75e
louis cobigo <louis.cobigo@gmail.com>
2023-04-21 14:58:07 +0200

    test reserve

--- index.js
+++ index.js
@@ -42,12 +42,9 @@ app.post("/api/stock/:productId/movement", async (req, res) => {
           const requestedQuantity = +quantity;
           if (requestedQuantity <= availableQuantity) {
             // La quantité demandée est disponible, soustraire la quantité réservée de la quantité disponible en stock
-            const disponible = stock[index].quantity -= requestedQuantity;
-            console.log(disponible)
-            stock[index]["disponible"] += disponible;
-            console.log(stock[index].disponible)
-            stock[index]["reserved"] += requestedQuantity;
-            console.log(stock[index].reserved)
+            const disponible = availableQuantity - requestedQuantity;
+            stock[index].disponible = disponible;
+            stock[index].reserved += requestedQuantity;
             res.status(200).send({ stock: stock });
           } else {
             // La quantité demandée n'est pas disponible, renvoyer une erreur
